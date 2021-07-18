cb67d975e16307971f97aa9369393328aea43d4d
geta-mac <ige1tah6iro1to0@gmail.com>
2021-07-19 07:58:28 +0900

    admin url をusers に追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_093828) do
+ActiveRecord::Schema.define(version: 2021_07_18_225129) do
 
   create_table "clubs", force: :cascade do |t|
     t.integer "club_name"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 2021_07_18_093828) do
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "url"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
