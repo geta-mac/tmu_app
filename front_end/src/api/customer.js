5df898dcee888d54ace1afb1d8ef109cf2036d95
geta-mac <ige1tah6iro1to0@gmail.com>
2021-07-31 21:42:41 +0900

    customerのほうのコメントアウトの解除忘れを修正

--- front_end/src/api/customer.js
+++ front_end/src/api/customer.js
@@ -1,33 +1,33 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
-// export function getCustomers(token) {
-//   return request({
-//     url: 'customers',
-//     method: 'get',
-//     params: { token }
-//   })
-// }
+export function getCustomers(token) {
+  return request({
+    url: 'customers',
+    method: 'get',
+    params: { token }
+  })
+}
 
-// export function createCustomer(data) {
-//   return request({
-//     url: 'customers',
-//     method: 'post',
-//     data
-//   })
-// }
+export function createCustomer(data) {
+  return request({
+    url: 'customers',
+    method: 'post',
+    data
+  })
+}
 
-// export function editCustomer(data) {
-//   return request({
-//     url: `customers/${data.id}`,
-//     method: 'put',
-//     data
-//   })
-// }
+export function editCustomer(data) {
+  return request({
+    url: `customers/${data.id}`,
+    method: 'put',
+    data
+  })
+}
 
-// export function deleteCustomer(data) {
-//   return request({
-//     url: `customers/${data.id}`,
-//     method: 'delete'
-//   })
-// }
+export function deleteCustomer(data) {
+  return request({
+    url: `customers/${data.id}`,
+    method: 'delete'
+  })
+}
 
