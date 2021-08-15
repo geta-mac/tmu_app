93685b5647bccc01698b88b1577c7e56a8c5595b
Shinichi Masuda <shinichi.masuda@staff.gu3.co.jp>
2021-08-16 08:12:13 +0900

    公開ページを設置できるように調整

--- front_end/src/views/public/index.vue
+++ front_end/src/views/public/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <p>public index</p>
+  <div>
+    <el-row>
+      <router-link :to="'/dashboard'" class="el-button el-button--primary">
+        <span>ログイン</span>
+      </router-link>
+    </el-row>
+    <p>public index</p>
+  </div>
 </template>
