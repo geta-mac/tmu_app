abea6020af215eb05dc54d24a3d64879602fe223
geta-mac <ige1tah6iro1to0@gmail.com>
2021-08-22 15:19:53 +0900

    サイドバー作成、高さ足りない

--- front_end/src/views/public/index.vue
+++ front_end/src/views/public/index.vue
@@ -5,21 +5,27 @@
         <span>ログイン</span>
       </router-link>
     </el-row>
-    <p>public index</p>
-    <PublicTable
-      :key="key"
-      :publics="publics"
-    />
+    <div class="aa">
+      <Sidebar />
+      <PublicTable
+        :key="key"
+        :publics="publics"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Sidebar from './components/Sidebar'
 import PublicTable from './components/PublicTable'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'Public',
-  components: { PublicTable },
+  components: {
+    PublicTable,
+    Sidebar
+  },
   data() {
     return {
       key: 0
@@ -35,3 +41,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.aa {
+  display: flex;
+}
+</style>
